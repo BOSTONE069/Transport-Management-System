4e191ead4328c36919d9ab6c983cfdf620a7559e
BOSTONE069 <bostoneochieng69@gmail.com>
2025-03-19 15:24:28 +0300

    Transport management system

---
+++ backend/trip/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Trip
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('current_location', 'pickup_location', 'dropoff_location', 'current_cycle_used')
+    search_fields = ('current_location', 'pickup_location', 'dropoff_location')
---
+++ backend/trip/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Trip(models.Model):
+    current_location = models.CharField(max_length=255)
+    pickup_location = models.CharField(max_length=255)
+    dropoff_location = models.CharField(max_length=255)
+    current_cycle_used = models.FloatField()
+
+    def __str__(self):
+        return f"Trip from {self.pickup_location} to {self.dropoff_location}"
